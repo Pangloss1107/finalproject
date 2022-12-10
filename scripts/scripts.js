3f9a197f1327c65f4ecacc725f10a34e1b563d66
Pangloss1107 <pablonsantos1107@gmail.com>
2022-12-10 02:30:32 -0300

    updates

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+//This is for Hamburguer Menu 
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open")
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+ }
+ 
+ const x = document.getElementById("hamburgerBtn")
+ x.onclick = toggleMenu;
