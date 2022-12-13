99bed09020fca339eb233508e1b30bc867d9ebfa
Pangloss1107 <pablonsantos1107@gmail.com>
2022-12-13 14:26:33 -0300

    upd

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -35,3 +35,52 @@ fetch(requestURL)
       document.querySelector("div#scroll").appendChild(clientsbox);
     }
   });
+
+
+
+
+//Footer Info 
+
+let monthnames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+
+];
+
+let daynames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+
+//To find and print the current Year
+const options = {weekday:"long", day:"numeric", month: "long", year: "numeric"};
+const d = new Date();
+const year = d.getFullYear();
+
+
+   
+//To show the las updated 
+let oLastModif = new Date(document.lastModified);
+let dayModif = oLastModif.getDate() 
+let monthModif = oLastModif.getMonth()
+let yearModif = oLastModif.getFullYear() 
+let hourModif = oLastModif.getHours()
+let minuteModif = oLastModif.getMinutes()
+let secondsModif = oLastModif.getSeconds()
+let completeMod = `Last Updated: ${dayModif}/${monthModif + 1}/${yearModif} ${hourModif}:${minuteModif}:${secondsModif}`
+document.getElementById("modified").textContent = completeMod;
